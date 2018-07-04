3bab398a29e18c8ff308ae6f270f3a6009180b31
Moisés Alejandro Navarro Presas <moisalejandro@gmail.com>
2018-07-03 21:21:15 -0500

    [IMP] cancel button if not obligatorio

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
 ##############################################################################
 {
     'name': 'Campos de porcentaje en listas de materiales',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'author': 'Humanytek',
     'category': 'Mrp',
     'description': """
